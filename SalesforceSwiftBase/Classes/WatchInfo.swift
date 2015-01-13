a7f57b3a6e2a9469ff8494cac04ec9a362097407
quintonwall <quinton_wall@hotmail.com>
2015-01-13 14:31:49 -0800

    add msg

--- SalesforceSwiftBase/Classes/WatchInfo.swift
+++ SalesforceSwiftBase/Classes/WatchInfo.swift
@@ -2,7 +2,8 @@
 //  Approvals.swift
 //  SalesforceWatch
 // 
-// Marshalling object to pass reply blocks back to the watch app
+// Marshalling object to pass reply blocks back to the watch app.
+// you shouldn't need to touch this
 //
 //  Created by Quinton Wall on 1/12/15.
 //  Copyright (c) 2015 Salesforce, Inc. All rights reserved.
