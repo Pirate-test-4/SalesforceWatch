655532d2007525f3452e8b0762b0caaffc63a166
quintonwall <qwall@salesforce.com>
2015-03-04 11:35:45 -0800

    Update Chevron.swift

--- SalesforceWatch WatchKit Extension/Chevron.swift
+++ SalesforceWatch WatchKit Extension/Chevron.swift
@@ -2,8 +2,8 @@
 //  RingHelper.swift
 //  SalesforceWatch
 // 
-// AppleWatch currently doesnt support animations. Let's make a little
-// wrapper to create a chevron-based ring graphic
+// 
+// wrapper to create a chevron-based ring graphic. TODO: use image animations in v2
 //
 //  Created by Quinton Wall on 1/13/15.
 //  Copyright (c) 2015 Salesforce, Inc. All rights reserved.
